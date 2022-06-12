6a9d934bd1aa0d7a0167429c6ea874e96817a95a
JetBrains CLion <76269679+JetBrains-CLion@users.noreply.github.com>
2022-06-12 05:13:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ async function Main() {
     await Uploader.Login("<EMAIL>", "<PASSWORD>")
     console.log("[YT-Uploader] Logged in.");
 
-    for (let i = 0; i < 10; i++) {
+    // Comment this if you want to upload more than 1 video
+    await Uploader.UploadVideo("<VIDEO FILE>", "<YOUTUBE TITLE>")
+    console.log("[YT-Uploader] Uploaded video.");
+    
+    // Uncomment this to upload more than 1 video
+    /*for (let i = 0; i < 10; i++) {
         await Uploader.UploadVideo("<VIDEO FILE>", "<YOUTUBE TITLE>")
         console.log(`[YT-Uploader] Uploaded video ${i + 1} time/times.`);
-    }
+    }*/
 
     console.log("[YT-Uploader] Finished uploading.");
     await Uploader.CloseBrowser()
